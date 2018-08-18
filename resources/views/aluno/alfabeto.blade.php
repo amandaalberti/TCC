ec9153f987f7f5d46bae3ddb0e4833c2d4a4d405
Amanda Alberti <amandavisualsoftware@gmail.com>
2018-08-18 18:58:17 -0300

    Alteração de Cores

--- resources/views/aluno/alfabeto.blade.php
+++ resources/views/aluno/alfabeto.blade.php
@@ -3,43 +3,43 @@
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-xs-12 text-center" style="margin: auto;">
-			<div class="letra btn-primary"><span>A</span></div>
-			<div class="letra btn-black"><span>B</span></div>
+			<div class="letra btn-rose"><span>A</span></div>
+			<div class="letra btn-cinza"><span>B</span></div>
 			<div class="letra btn-warning"><span>C</span></div>
 			<div class="letra btn-danger"><span>D</span></div>
-			<div class="letra btn-success"><span>E</span></div>
+			<div class="letra btn-verde"><span>E</span></div>
 			<div class="letra btn-info"><span>F</span></div>
 			<div class="letra btn-pink"><span>G</span></div>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-xs-12 text-center" style="margin: auto;">
-			<div class="letra btn-primary"><span>H</span></div>
-			<div class="letra btn-black"><span>I</span></div>
-			<div class="letra btn-warning"><span>J</span></div>
-			<div class="letra btn-danger"><span>K</span></div>
+			<div class="letra btn-oceano"><span>H</span></div>
+			<div class="letra btn-roxo"><span>I</span></div>
+			<div class="letra btn-tomato"><span>J</span></div>
+			<div class="letra btn-cinza"><span>K</span></div>
 			<div class="letra btn-success"><span>L</span></div>
-			<div class="letra btn-info"><span>M</span></div>
+			<div class="letra btn-roxo"><span>M</span></div>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-xs-12 text-center" style="margin: auto;">
 			<div class="letra btn-pink"><span>N</span></div>
-			<div class="letra btn-primary"><span>O</span></div>
-			<div class="letra btn-black"><span>P</span></div>
+			<div class="letra btn-verde"><span>O</span></div>
+			<div class="letra btn-cinza"><span>P</span></div>
 			<div class="letra btn-warning"><span>Q</span></div>
-			<div class="letra btn-danger"><span>R</span></div>
-			<div class="letra btn-success"><span>S</span></div>
-			<div class="letra btn-info"><span>T</span></div>
+			<div class="letra btn-roxo"><span>R</span></div>
+			<div class="letra btn-rose"><span>S</span></div>
+			<div class="letra btn-blue"><span>T</span></div>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-xs-12 text-center" style="margin: auto;">
-			<div class="letra btn-pink"><span>U</span></div>
+			<div class="letra btn-roxo"><span>U</span></div>
 			<div class="letra btn-primary"><span>V</span></div>
-			<div class="letra btn-black"><span>W</span></div>
-			<div class="letra btn-warning"><span>X</span></div>
-			<div class="letra btn-danger"><span>Y</span></div>
+			<div class="letra btn-danger"><span>W</span></div>
+			<div class="letra btn-oceano"><span>X</span></div>
+			<div class="letra btn-tomato"><span>Y</span></div>
 			<div class="letra btn-success"><span>Z</span></div>
 		</div>
 	</div>
