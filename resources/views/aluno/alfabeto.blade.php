770aff96c562b805d47cb4dfe88567bc9b362930
Amanda Alberti <amandavisualsoftware@gmail.com>
2018-08-18 17:12:32 -0300

    Criação da tela do alfabeto

---
+++ resources/views/aluno/alfabeto.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+@section('content')
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-xs-12 text-center" style="margin: auto;">
+			<div class="letra"><span>A</span></div>
+			<div class="letra"><span>B</span></div>
+			<div class="letra"><span>C</span></div>
+			<div class="letra"><span>D</span></div>
+			<div class="letra"><span>E</span></div>
+			<div class="letra"><span>F</span></div>
+			<div class="letra"><span>G</span></div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12 text-center" style="margin: auto;">
+			<div class="letra"><span>H</span></div>
+			<div class="letra"><span>I</span></div>
+			<div class="letra"><span>J</span></div>
+			<div class="letra"><span>K</span></div>
+			<div class="letra"><span>L</span></div>
+			<div class="letra"><span>M</span></div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12 text-center" style="margin: auto;">
+			<div class="letra"><span>N</span></div>
+			<div class="letra"><span>O</span></div>
+			<div class="letra"><span>P</span></div>
+			<div class="letra"><span>Q</span></div>
+			<div class="letra"><span>R</span></div>
+			<div class="letra"><span>S</span></div>
+			<div class="letra"><span>T</span></div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12 text-center" style="margin: auto;">
+			<div class="letra"><span>U</span></div>
+			<div class="letra"><span>V</span></div>
+			<div class="letra"><span>W</span></div>
+			<div class="letra"><span>X</span></div>
+			<div class="letra"><span>Y</span></div>
+			<div class="letra"><span>Z</span></div>
+		</div>
+	</div>
+</div>
+@endsection
