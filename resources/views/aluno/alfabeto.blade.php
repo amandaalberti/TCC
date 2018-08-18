4d9898228ab231774b89ec0f218518397df16563
Amanda Alberti <amandavisualsoftware@gmail.com>
2018-08-18 17:35:18 -0300

    Cores das letras do alfabeto

--- resources/views/aluno/alfabeto.blade.php
+++ resources/views/aluno/alfabeto.blade.php
@@ -3,44 +3,44 @@
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-xs-12 text-center" style="margin: auto;">
-			<div class="letra"><span>A</span></div>
-			<div class="letra"><span>B</span></div>
-			<div class="letra"><span>C</span></div>
-			<div class="letra"><span>D</span></div>
-			<div class="letra"><span>E</span></div>
-			<div class="letra"><span>F</span></div>
-			<div class="letra"><span>G</span></div>
+			<div class="letra btn-primary"><span>A</span></div>
+			<div class="letra btn-black"><span>B</span></div>
+			<div class="letra btn-warning"><span>C</span></div>
+			<div class="letra btn-danger"><span>D</span></div>
+			<div class="letra btn-success"><span>E</span></div>
+			<div class="letra btn-info"><span>F</span></div>
+			<div class="letra btn-pink"><span>G</span></div>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-xs-12 text-center" style="margin: auto;">
-			<div class="letra"><span>H</span></div>
-			<div class="letra"><span>I</span></div>
-			<div class="letra"><span>J</span></div>
-			<div class="letra"><span>K</span></div>
-			<div class="letra"><span>L</span></div>
-			<div class="letra"><span>M</span></div>
+			<div class="letra btn-primary"><span>H</span></div>
+			<div class="letra btn-black"><span>I</span></div>
+			<div class="letra btn-warning"><span>J</span></div>
+			<div class="letra btn-danger"><span>K</span></div>
+			<div class="letra btn-success"><span>L</span></div>
+			<div class="letra btn-info"><span>M</span></div>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-xs-12 text-center" style="margin: auto;">
-			<div class="letra"><span>N</span></div>
-			<div class="letra"><span>O</span></div>
-			<div class="letra"><span>P</span></div>
-			<div class="letra"><span>Q</span></div>
-			<div class="letra"><span>R</span></div>
-			<div class="letra"><span>S</span></div>
-			<div class="letra"><span>T</span></div>
+			<div class="letra btn-pink"><span>N</span></div>
+			<div class="letra btn-primary"><span>O</span></div>
+			<div class="letra btn-black"><span>P</span></div>
+			<div class="letra btn-warning"><span>Q</span></div>
+			<div class="letra btn-danger"><span>R</span></div>
+			<div class="letra btn-success"><span>S</span></div>
+			<div class="letra btn-info"><span>T</span></div>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-xs-12 text-center" style="margin: auto;">
-			<div class="letra"><span>U</span></div>
-			<div class="letra"><span>V</span></div>
-			<div class="letra"><span>W</span></div>
-			<div class="letra"><span>X</span></div>
-			<div class="letra"><span>Y</span></div>
-			<div class="letra"><span>Z</span></div>
+			<div class="letra btn-pink"><span>U</span></div>
+			<div class="letra btn-primary"><span>V</span></div>
+			<div class="letra btn-black"><span>W</span></div>
+			<div class="letra btn-warning"><span>X</span></div>
+			<div class="letra btn-danger"><span>Y</span></div>
+			<div class="letra btn-success"><span>Z</span></div>
 		</div>
 	</div>
 </div>
